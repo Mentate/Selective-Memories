8e8754d4ec40a9270f38a73f851ca84921a570c9
Dad <josiah@josiahcarpenter.com>
2025-04-26 16:27:22 -0500

    file browser can be minimized

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 import pygame
 import constants
 from windows import (
-    ClickableAsset
+    ClickableAsset,
+    AssetGroup
 )
 
 # pygame setup
@@ -13,37 +14,67 @@ clock = pygame.time.Clock()
 running = True
 
 
-active_assets = {
-    "file_window":ClickableAsset("assets/byte_navigator.png",0,0),
-    "chat_wndow":ClickableAsset("assets/chat_window.png",735, 0),
-    "recovery_window":ClickableAsset("assets/recovery_window.png", 735, 372),
-    "taskbar":ClickableAsset("assets/bar.png", 0,737),
-    "tbar_fldr":ClickableAsset("assets/folder_icon.png",100,737)
+# handlers here at the top, may move to another module at some point
+def test_handler1(event):
+    print("handler 1 worked")
+
+def min_file_win(name, asset_data, event):
+    if file_window.visible == True:
+        file_window.visible = False
+    else:
+        file_window.visible = True
+    print("handler 2 worked")
+
+
+
+# create the primary assets for our asset groups
+osbar_background = ClickableAsset("assets/bar.png", (0,737),test_handler1)
+chat_window_background = ClickableAsset("assets/chat_window.png",(735, 0),test_handler1)
+recovery_window_background = ClickableAsset("assets/recovery_window.png", (735,372), test_handler1)
+file_window_background = ClickableAsset("assets/byte_navigator.png",(0,0),test_handler1)
+
+# start building our asset groups
+osbar = AssetGroup(osbar_background)
+chat_window = AssetGroup(chat_window_background)
+recovery_window = AssetGroup(recovery_window_background)
+file_window = AssetGroup(file_window_background)
+
+# add child assets to the groups
+osbar_fldr = ClickableAsset("assets/folder_icon.png",(0,0),min_file_win)
+osbar.add_asset("folder",osbar_fldr,True,(90,0))
+
+# add them to a dict
+asset_groups = {
+    "osbar":osbar,
+    "file_win":file_window,
+    "chat_window":chat_window,
+    "recovery_window":recovery_window
 }
 
 
-
 while running:
-    for asset in active_assets:
-        active_assets[asset].draw(screen)
+    screen.fill((0,0,0))
+    for group in asset_groups:
+        asset_groups[group].draw(screen)
 
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.MOUSEBUTTONDOWN:
-            # left_click = pygame.mouse.get_pressed()[0]
-            # middle_click = pygame.mouse.get_pressed()[1]
-            # right_click = pygame.mouse.get_pressed()[2]
             x, y = pygame.mouse.get_pos()
-            for asset in active_assets:
-                if active_assets[asset].clicked(event):
-                    print(f"{asset} was clicked")
+            for group in asset_groups:
+                asset_name = asset_groups[group].clicked(event)
+                if asset_name:
+                    print(f"{asset_name} was clicked")
 
 
     pygame.display.flip()
     clock.tick(30)  # limits FPS to 60
 
 pygame.quit()
+
+
+            # left_click = pygame.mouse.get_pressed()[0]
+            # middle_click = pygame.mouse.get_pressed()[1]
+            # right_click = pygame.mouse.get_pressed()[2]
--- windows.py
+++ windows.py
@@ -3,10 +3,11 @@ import constants
 
 
 class ClickableAsset:
-    def __init__(self,asset_path,x,y):
+    def __init__(self,asset_path:str,loc:tuple,handler):
         self.bg_image = pygame.image.load(asset_path).convert()
-        self.x = x
-        self.y = y
+        self.x = loc[0]
+        self.y = loc[1]
+        self.handler = handler
 
 
     def draw(self,screen):
@@ -20,5 +21,43 @@ class ClickableAsset:
 
 
 class AssetGroup:
-    def __init__(self):
-        pass
+    def __init__(self,primary_asset:ClickableAsset):
+        self.primary_asset = primary_asset
+        self.sub_assets = {}
+        self.visible = True
+
+    def clicked(self,event):
+        for asset_name in self.sub_assets:
+            asset_data = self.sub_assets[asset_name]
+            asset_obj = asset_data["asset_obj"]
+            if asset_data["visible"] == True:
+                result = asset_obj.clicked(event)
+                if result == True:
+                    asset_obj.handler(asset_name, asset_data, event) # call the handler
+                    return asset_name
+
+    def add_asset(self, name:str, asset_obj:ClickableAsset, visible:bool, relative_loc:tuple,meta=None):
+        self.sub_assets[name] = {
+            'asset_obj':asset_obj,
+            'visible':visible,
+            'relative_loc':relative_loc,
+            'meta':meta
+        }
+
+    def draw(self,screen):
+        # draw the primary first
+        if self.visible == True:
+            self.primary_asset.draw(screen)
+            pri_x = self.primary_asset.x
+            pri_y = self.primary_asset.y
+
+            # iterate through sub assets and draw them
+            for item in self.sub_assets:
+                # our dict stores relative location, that is x and y relative to the top left of the primary
+                # asset. we need to update the absolute locations of each asset before we draw it
+                asset_data = self.sub_assets[item]
+                asset_obj = asset_data["asset_obj"]
+                if asset_data["visible"] == True:
+                    asset_obj.x = asset_data["relative_loc"][0] + pri_x
+                    asset_obj.y = asset_data["relative_loc"][1] + pri_y
+                    asset_obj.draw(screen)
